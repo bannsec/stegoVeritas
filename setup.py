42aabeb4ed8f71a0984ff2665e9a0446dd3ca1a7
bannsec <whootandahalf@gmail.com>
2019-02-18 06:03:02 +0000

    Removing pfp due to 'six' requirement issues

--- setup.py
+++ setup.py
@@ -12,6 +12,8 @@ with open('stegoveritas/version.py') as f:
     exec(f.read())
 
 # TODO: Get 'six' package up to newest version. Right now py010parser is forcing it to 1.10.0, which isn't great.
+# NOTE: Removing pfp package for now, until I need it. If I do end up needing it, i will need to have my own versions of both pfp and py010parser
+# Specifically, I need to modify py010parser to not be stuck with an old "six" version (which is causing issues), then updated pfp to point to my version of py010parser.
 # https://github.com/d0c-s4vage/py010parser/pull/19
 
 setup(
@@ -33,9 +35,9 @@ setup(
     ],
     keywords='steg stego steganography stegoveritas',
     packages=find_packages(exclude=['contrib', 'docs', 'tests', 'dist']),
-    install_requires=['pillow', 'numpy', 'pfp', 'python-magic', 'prettytable', 'exifread', 'python-xmp-toolkit', 'stegoveritas-binwalk'],
+    install_requires=['pillow', 'numpy', 'python-magic', 'prettytable', 'exifread', 'python-xmp-toolkit', 'stegoveritas-binwalk'],
     extras_require={
-        'dev': ['six==1.10.0','ipython','twine','pytest','python-coveralls','coverage','pytest-cov','pytest-xdist','sphinxcontrib-napoleon', 'sphinx_rtd_theme','sphinx-autodoc-typehints', 'pyOpenSSL'],
+        'dev': ['ipython','twine','pytest','python-coveralls','coverage','pytest-cov','pytest-xdist','sphinxcontrib-napoleon', 'sphinx_rtd_theme','sphinx-autodoc-typehints', 'pyOpenSSL'],
     },
     entry_points={
         'console_scripts': [
