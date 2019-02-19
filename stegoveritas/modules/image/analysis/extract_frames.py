4888a309c160fb1ed0e40a2e440e5e2b9baffbb7
bannsec <whootandahalf@gmail.com>
2019-02-19 02:39:58 +0000

    Adding gif/apng frame extraction

--- stegoveritas/modules/image/analysis/extract_frames.py
+++ stegoveritas/modules/image/analysis/extract_frames.py
@@ -5,6 +5,12 @@ logger = logging.getLogger('StegoVeritas:Modules:Image:Analysis:ExtractFrames')
 import os
 from PIL import Image
 
+import png
+from copy import copy
+from struct import pack, unpack
+
+from apng import APNG
+
 def run(image):
     """Extracts individual frames from an animated gif.
 
@@ -23,6 +29,149 @@ def run(image):
     if not args.auto and not args.extract_frames:
         logger.debug('Nothing to do.')
         return
+
+    if image.file.format == 'PNG':
+        extract_png(image)
+
+    elif image.file.format == 'GIF':
+        extract_gif(image)
+
+
+def extract_png(image):
+
+    # TODO: Maybe just use APNG: https://pypi.org/project/apng/
+
+    img = APNG.open(image.veritas.file_name)
+
+    if len(img.frames) == 1:
+        logger.debug('Only one frame detected.')
+        return
+
+    print("Extracting {} PNG frames.".format(len(img.frames)))
+    for i, (png, control) in enumerate(img.frames):
+        outfile = os.path.join(image.veritas.results_directory, '{}_frame_{}.png'.format(os.path.basename(image.veritas.file_name), i))
+        png.save(outfile)
+
+    return
+
+    ### Below is some original work before finding that library
+    
+    # PIL apparently can't handle this. Neither can ffmpeg. ImageMagick can be fooled.
+    # Time to write my own animated png extractor
+
+    img = png.Reader(image.veritas.file_name)
+    chunks = list(img.chunks())
+
+    num_frames = len(list(x for x in chunks if x[0] in [b'fcTL']))
+    
+    """
+    actl = next(x for x in chunks if x[0] == b'acTL')
+
+    # How many frames does the image claim to have
+    given_num_frames, _ = unpack('>II', actl[1])
+
+    if given_num_frames != num_frames:
+        print('Image only claims {} frames, but I discovered {}'.format(given_num_frames, num_frames))
+        print('Patching and attempting to extract the frames')
+
+        new_actl = (b'acTL', pack('>II',num_frames,0))
+
+        new_chunks = []
+        frame_number = 0
+        for t,v in chunks:
+            if t == b'acTL':
+                new_chunks.append(new_actl)
+
+            elif t == b'fcTL':
+                new_chunks.append((t, pack('>I', frame_number) + v[4:]))
+                frame_number += 1
+
+            else:
+                new_chunks.append((t,v))
+
+        with open('blerg.png','wb') as f:
+            png.write_chunks(f, new_chunks)
+
+    return
+    """
+
+    #######
+
+    if num_frames == 1:
+        logger.debug('Only one frame discovered.')
+        return
+
+    print('Discovered multuple PNG frames. Attempting to extract them...')
+
+    header_chunk = next(x for x in chunks if x[0] in [b'IHDR'])
+    end_chunk = (b'IEND', b'')
+    actl_chunk = (b'acTL', b'\x00\x00\x00\x01\x00\x00\x00\x00') # looping, 1 frame
+
+    new_image = []
+    num_frames = 0
+
+    # Type and value for the chunks
+    for t, val in chunks:
+        print(t)
+        
+        # We're done with this image
+        if t in [b'fcTL', b'IEND']:
+
+            # We have something to save
+            if new_image != []:
+
+                # Put propper chunks in place
+                new_image.insert(0, copy(actl_chunk))
+                new_header_chunk = list(copy(header_chunk))
+                #new_header_chunk[1] = pack('>II', width, height) + new_header_chunk[1][8:]
+                #new_header_chunk[1] = width_height + new_header_chunk[1][8:]
+                new_header_chunk = tuple(new_header_chunk)
+                new_image.insert(0, new_header_chunk)
+                #new_image.insert(0, copy(header_chunk))
+                new_image.append(copy(end_chunk))
+                print(new_image)
+
+                outfile = os.path.join(image.veritas.results_directory, '{}_frame_{}.png'.format(os.path.basename(image.veritas.file_name), num_frames))
+                print(outfile)
+
+                with open(outfile, 'wb') as f:
+                    png.write_chunks(f, new_image)
+
+                #new_image = [(t, val)]
+                new_image = []
+                num_frames += 1
+
+            if t == b'fcTL':
+                # Update our frame size
+                #width, height = unpack('>II', val[4:12])
+                #print('width and height: ' + str(width) + ' ' + str(height))
+                #width_height = val[4:12]
+                #new_image.append((t, b'\x00\x00\x00\x00' + val[4:]))
+                new_image.append((t, val))
+
+            # Nothing to save, just add our header in
+            #else:
+            #    new_image.append((t, val))
+
+        elif t == b'IDAT':
+            new_image.append((t, val))
+
+        elif t == b'fdAT':
+            # fdAT is simply IDAT with 4 bytes at the beginning
+            new_image.append((b'IDAT', val[4:]))
+
+
+        """
+        # Chunk data between image designators
+        elif t in [b'IDAT', b'fdAT']:
+            new_image.append((t, val))
+        """
+
+
+
+    
+
+def extract_gif(image):
 
     if not hasattr(image.file, 'is_animated') or image.file.is_animated is False:
         logger.debug('This file is not an animated image.')
--- tests/images/test_extract_frames.py
+++ tests/images/test_extract_frames.py
@@ -26,3 +26,19 @@ def test_extract_frames_gif():
 
         for f in frames:
             assert SVImage.hash_file(os.path.join(tmpdirname, f)) in expected_hashes
+
+def test_extract_frames_apng():
+
+    with tempfile.TemporaryDirectory() as tmpdirname:  
+        args = [os.path.join(SCRIPTDIR, 'Animated_PNG_example_bouncing_beach_ball.png'), '-out', tmpdirname, '-extract_frames'] 
+        veritas = stegoveritas.StegoVeritas(args=args)
+        veritas.run()
+
+        frames = glob(os.path.join(tmpdirname, '*_frame_*.png'))
+
+        assert len(frames) == 20
+
+        expected_hashes = ['4984cd34d8f67a060cc3ea0fab8fda80fe32e78e2290d3692d2681657d2674bb', '46bff92f931cf247608bdb68b541d14559e7968969de2192190b0a65f735cde4', 'aab92ada01f7ccd13c5a2c31296385a2bb1cc2e94243f2041aac0808883b5cbe', 'cb5dc80b7f386daf0fffbda763f2c5465036ff38fbb76cd7ecfbcb829824006a', '0b524223c49b671df97dacaa8807acf0fdade046fb2bdd3e7c2309457ca5004f', 'ad6b6c9cbe1f15c803356a4296f94c895c3747aaca8f977a272a83d6ebfdf676', '379f74e77a2a2bf2a27a2d38b109a7b28decf6d577619f4117441afc21b01e6c', 'd4048ecf441fb11402996ccb7f33fbfe2d61e1660d9caa17114c245b44f3a629', 'cc62e051325746579f9fa913766e4b872f790a37b539e6c710d9cb4535f2ad16', '2d8924862326066b46a6abef2aa61e150e6c933f57ec8280b83d8297d05be793', '2619bf1ef0685aa3c9f8745393d43963f9f3c37021c8381ceabeae3038e0af1e', 'fa8db31b7a6ac3b96705d405cd9e3dc04b2b8d1b9dce24219db937c1f4c8ddaa', '3876ab9e1a14a8b15c3e7e1b58474f0c35a9368c0a2dd149a3b226af3cc469c8', 'd94d61882216ae41e212e7e21994cea6ba195964046eb2399723331abefbe3b3', 'ff796a3ca22fca14819ebf0c6722dfa9b1fe7837396257b8fd9d05093398aee6', '622873b75685aa17a060fa5bbb7fc2d3368e9a6260f7394f68ccd01efb6a9a3a', 'a6770e6b857c0d6d34b41c49b96eb9e5cf42a213d042b9c3ff79117bd9b716a2', 'd362b4cf63f2a1012cb0e2d17d3b263843d28136deb340edec4fbc2774e666b0', '4ba41bd812ca08aaec3f60a07ec765ec8937602d53222b7b0fce46b9438a11c3', 'e859f886e3b65ec4e8f6cb14d31f32c36d1a8bf9a276c4f838f76dfae6ddf38d']
+
+        for f in frames:
+            assert SVImage.hash_file(os.path.join(tmpdirname, f)) in expected_hashes
