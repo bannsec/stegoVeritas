beaca949813f966c22009ddfc9322e2b3b1ca8bb
samarth <samarth.kathal@gmail.com>
2019-05-06 08:17:43 +0530

    added change directory function for configs

--- stegoveritas/config.py
+++ stegoveritas/config.py
@@ -7,6 +7,20 @@ SCRIPTDIR = os.path.dirname(os.path.realpath(__file__))
 PWD = os.getcwd()
 
 TEMPFILE = os.path.join(SCRIPTDIR,"stegoveritas.temp")
-# TODO: Use python to make this dynamic
+
 RESULTSDIR = os.path.join(PWD,"results")
+
+# TODO: Use python to make this dynamic                                                                                                        ✔  7513  08:12:12
+
+a = input("ENTER ABSOLUTE OR RELATIVE PATH: ")
+try:
+    a = str(a)
+except:
+    print("DEBUG")
+    quit()
+try:
+    PWD = os.chdir(a)
+except:
+    print("DEBUG")
+    quit()
 
