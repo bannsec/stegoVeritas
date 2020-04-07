fe23830b7a8bbaa6ba4d9b1e960620cecb492c5d
bannsec <whootandahalf@gmail.com>
2020-04-07 15:06:42 -0400

    Multi-processing filters

--- stegoveritas/modules/image/analysis/filters.py
+++ stegoveritas/modules/image/analysis/filters.py
@@ -3,9 +3,75 @@ import logging
 logger = logging.getLogger('StegoVeritas:Modules:Image:Analysis:Filters')
 
 import os
-from PIL import Image,ImageFilter,ImageFile
+from PIL import Image, ImageFilter, ImageFile
 import numpy
 from copy import copy
+import multiprocessing
+
+def run_filter(image, f_a, filt):
+    try:
+        g = f_a.filter(filt)
+    except IOError as e:
+        # Generically print it, then try again (truncated error)
+        logger.error("%s", e)
+        ImageFile.LOAD_TRUNCATED_IMAGES = True
+        g = f_a.filter(filt)
+
+    g.save(os.path.join(image.veritas.results_directory, os.path.basename(image.veritas.file_name) + "_" + filt.name.replace(" ","_") + ".png"))
+
+def run_invert(image, f_a):
+    g = numpy.array(f_a)
+    g = g ^ 0xff
+    g = Image.fromarray(g)
+    g.save(os.path.join(image.veritas.results_directory, os.path.basename(image.veritas.file_name) + "_inverted.png"))
+
+def run_color_planes(image, f_a, plane):
+
+    if plane == "red":
+        g = numpy.array(f_a)
+        g[:,:,2] *= 0
+        g[:,:,1] *= 0
+        g = Image.fromarray(g)
+        g.save(os.path.join(image.veritas.results_directory, os.path.basename(image.veritas.file_name) + "_red_plane.png"))
+    
+    elif plane == "green":
+        g = numpy.array(f_a)
+        g[:,:,2] *= 0
+        g[:,:,0] *= 0
+        g = Image.fromarray(g)
+        g.save(os.path.join(image.veritas.results_directory, os.path.basename(image.veritas.file_name) + "_green_plane.png"))
+
+    elif plane == "blue":
+        g = numpy.array(f_a)
+        g[:,:,0] *= 0
+        g[:,:,1] *= 0
+        g = Image.fromarray(g)
+        g.save(os.path.join(image.veritas.results_directory, os.path.basename(image.veritas.file_name) + "_blue_plane.png"))
+
+    elif plane == "alpha":
+        # TODO: Still assuming all the positions for the given colors in the tuple
+        if "A" in image.file.getbands():
+                g = numpy.array(image.file)
+                g[:,:,0] *= 0
+                g[:,:,1] *= 0
+                g[:,:,2] *= 0
+                g = Image.fromarray(g)
+                g.save(os.path.join(image.veritas.results_directory, os.path.basename(image.veritas.file_name) + "_alpha_plane.png"))
+
+
+def run_bit_plane(image, f_a, plane, color, index):
+    """Extracts a specific bit plane from the image (i.e.: "Green" bit 0)"""
+
+    temp = numpy.array(plane)
+    # Shift over to get right offset
+    temp = temp >> index
+    # Get the bit
+    temp = temp & 1
+    # Fix-up the background
+    temp[temp == 1] = 255
+    # Save it off
+    temp = Image.fromarray(temp)
+    temp.save(os.path.join(image.veritas.results_directory, os.path.basename(image.veritas.file_name) + "_{0}_{1}.png".format(color,index)))
 
 def run(image):
     """Performs various graphical filters on the image.
@@ -43,93 +109,55 @@ def run(image):
         logger.debug('Image is colormap, skipping.')
         return
 
+    try:
+        colorPlanes = image.file.split()
+    except OSError as e:
+        logger.error("%s", e)
+        ImageFile.LOAD_TRUNCATED_IMAGES = True
+        colorPlanes = image.file.split()
+
     ###########
     # Filters #
     ###########
     
     # Built-in filters to apply
     filters = [ImageFilter.EDGE_ENHANCE, ImageFilter.EDGE_ENHANCE_MORE, ImageFilter.FIND_EDGES, ImageFilter.MaxFilter, ImageFilter.MedianFilter, ImageFilter.MinFilter, ImageFilter.ModeFilter, ImageFilter.SHARPEN, ImageFilter.SMOOTH, ImageFilter.GaussianBlur]
+    wait_for = []
+
+    with multiprocessing.Pool() as pool:
+
+        # Generic filters
+        for filt in filters:
+            wait_for.append(pool.apply_async(run_filter, args=(image, f_a, filt)))
+
+        # Specific color planes
+        for plane in ["red", "green", "blue", "alpha"]:
+            wait_for.append(pool.apply_async(run_color_planes, args=(image, f_a, plane)))
     
-    for filt in filters:
-        try:
-            g = f_a.filter(filt)
-        except IOError as e:
-            # Generically print it, then try again (truncated error)
-            logger.error("%s", e)
-            ImageFile.LOAD_TRUNCATED_IMAGES = True
-            g = f_a.filter(filt)
+        ##################
+        # Bit Map Planes #
+        ##################
+        
+        # TODO: This is an assumption on ordering... Maybe find a better way.
+        color = "Red"
+        for plane in colorPlanes:
+                for i in range(8):
+                    # run_bit_plane(image, f_a, color, index
+                    wait_for.append(pool.apply_async(run_bit_plane, args=(image, f_a, plane, color, i)))
+                # Move to next color
+                if color == "Red":
+                        color = "Green"
+                elif color == "Green":
+                        color = "Blue"
+                elif color == "Blue":
+                        color = "Alpha"
+                elif color == "Alpha":
+                        color = "Error"
+                else:
+                        raise Exception("We got more planes than we were expecting...\nPlanes: {0}".format(colorPlanes))
 
-        g.save(os.path.join(image.veritas.results_directory, os.path.basename(image.veritas.file_name) + "_" + filt.name.replace(" ","_") + ".png"))
-    
-    ################
-    # Color planes #
-    ################
-    g = numpy.array(f_a)
-    g[:,:,2] *= 0
-    g[:,:,1] *= 0
-    g = Image.fromarray(g)
-    g.save(os.path.join(image.veritas.results_directory, os.path.basename(image.veritas.file_name) + "_red_plane.png"))
-    
-    g = numpy.array(f_a)
-    g[:,:,2] *= 0
-    g[:,:,0] *= 0
-    g = Image.fromarray(g)
-    g.save(os.path.join(image.veritas.results_directory, os.path.basename(image.veritas.file_name) + "_green_plane.png"))
+        # Inverting
+        wait_for.append(pool.apply_async(run_invert, args=(image, f_a)))
 
-    g = numpy.array(f_a)
-    g[:,:,0] *= 0
-    g[:,:,1] *= 0
-    g = Image.fromarray(g)
-    g.save(os.path.join(image.veritas.results_directory, os.path.basename(image.veritas.file_name) + "_blue_plane.png"))
-
-    # TODO: Still assuming all the positions for the given colors in the tuple
-    if "A" in bands:
-            g = numpy.array(image.file)
-            g[:,:,0] *= 0
-            g[:,:,1] *= 0
-            g[:,:,2] *= 0
-            g = Image.fromarray(g)
-            g.save(os.path.join(image.veritas.results_directory, os.path.basename(image.veritas.file_name) + "_alpha_plane.png"))
-
-    
-    ##################
-    # Bit Map Planes #
-    ##################
-    colorPlanes = image.file.split()
-    
-    # TODO: This is an assumption on ordering... Maybe find a better way.
-    color = "Red"
-    for plane in colorPlanes:
-            for i in range(8):
-                    temp = numpy.array(plane)
-                    # Shift over to get right offset
-                    temp = temp >> i
-                    # Get the bit
-                    temp = temp & 1
-                    # Fix-up the background
-                    temp[temp == 1] = 255
-                    # Save it off
-                    temp = Image.fromarray(temp)
-                    temp.save(os.path.join(image.veritas.results_directory, os.path.basename(image.veritas.file_name) + "_{0}_{1}.png".format(color,i)))
-            # Move to next color
-            if color == "Red":
-                    color = "Green"
-            elif color == "Green":
-                    color = "Blue"
-            elif color == "Blue":
-                    color = "Alpha"
-            elif color == "Alpha":
-                    color = "Error"
-            else:
-                    raise Exception("We got more planes than we were expecting...\nPlanes: {0}".format(colorPlanes))
-
-    ##########
-    # Invert #
-    ##########
-    g = numpy.array(f_a)
-    g = g ^ 0xff
-    g = Image.fromarray(g)
-    g.save(os.path.join(image.veritas.results_directory, os.path.basename(image.veritas.file_name) + "_inverted.png"))
-
-
-
+        # Wait for everything to finish
+        for w in wait_for: w.wait()
